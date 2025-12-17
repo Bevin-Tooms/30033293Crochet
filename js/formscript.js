d2d9c57f4e63a5e4d4bdd345fd742e2ffed4bc3a
Bevin-Tooms <30033293@live.nwrc.ac.uk>
2025-12-17 13:19:47 +0000

    Add files via upload

---
+++ js/formscript.js
@@ -0,0 +1,39 @@
+function myFormSubmits(event) {
+    event.preventDefault(); // Prevent the default form submission
+    console.log("Thank you test")
+    // Call the custom fuctions you want to execute before submitting the form
+    downloadFormData();
+    thanks();
+}
+
+function downloadFormData() {
+    const form = document.getElementById('myForm');
+    const formData = new FormData(form);
+    let dataString = '';
+
+    for (let [key, value] of formData.entries()) {
+        dataString += '${key}: ${value}\n';
+    }
+
+    // Create a Blob from the data string
+    const blob = new Blob([dataString], {
+        type: 'text/plan'
+    });
+    const url = URL.createObjectURL(blob);
+
+    // Create a Blob from the data string
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = 'form-data.txt';
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+
+    // Revoke the object URL after download
+    URL.revokeObjectURL(url);
+}
+
+function thanks() {
+    // Redirect to another page after download
+    window.location.href = 'thankyou.html'; // Replace with your desired URL
+}
